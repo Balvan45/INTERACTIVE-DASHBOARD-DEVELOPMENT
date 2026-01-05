01edf4a030b204f90e30702c9d2e5cd190a17086
Balvan45 <ashwinbalvan2004@gmail.com>
2026-01-05 20:52:36 +0530

    Update dashBoard.py

--- dashBoard.py
+++ dashBoard.py
@@ -3,13 +3,11 @@ import dash
 from dash import dcc, html
 import plotly.express as px
 
-# Load dataset
 df = pd.read_csv("sales_data.csv")
 
-# Create Dash app
 app = dash.Dash(__name__)
 
-# Visualizations
+
 region_sales = df.groupby("region")["sales"].sum().reset_index()
 region_fig = px.bar(region_sales, x="region", y="sales",
                     title="Total Sales by Region", color="region")
@@ -22,7 +20,6 @@ top_sales = df.sort_values(by="sales", ascending=False).head(5)
 top_fig = px.bar(top_sales, x="product", y="sales",
                  title="Top 5 Sales Records")
 
-# Layout
 app.layout = html.Div([
     html.H1("Interactive Sales Analytics Dashboard",
             style={"textAlign": "center"}),
@@ -32,6 +29,5 @@ app.layout = html.Div([
     dcc.Graph(figure=top_fig)
 ])
 
-# Run app
 if __name__ == "__main__":
     app.run_server(debug=True)
